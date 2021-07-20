58a4691f5a2c3b4fb2fdee6670c671fc3ff48870
Whacky0 <62712306+Whacky0@users.noreply.github.com>
2021-07-19 23:00:20 -0300

    Tp-Ultimo Avance

---
+++ TP/Game/Enemies/EnemyShip.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using Engine;
+using Engine.Utils;
+
+namespace Game
+{
+    public class EnemyShip : GameObject
+    {
+        private static Random rnd = new Random();
+
+        private int shipIndex;
+        private EnemyBehavior behavior;
+        
+        public EnemyShip(int shipIndex, EnemyBehavior behavior)
+        {
+            this.shipIndex = shipIndex;
+            this.behavior = behavior;
+            this.MiImagen = LoadImage();
+            
+            Visible = false;
+        }
+
+        public PlayerShip Player
+        {
+            get
+            {
+                return AllObjects
+                    .Select(obj => obj as PlayerShip)
+                    .FirstOrDefault(obj => obj != null);
+            }
+        }
+
+        public EnemyBehavior Behavior
+        {
+            get { return behavior; }
+        }
+
+        public override void Update(float deltaTime)
+        {
+            if (Position.X <= 0)//Eliminar los enemigos cuando salen de la pantalla
+            {
+
+                this.Delete();
+                return;
+            }
+
+            behavior.Update(this, deltaTime);
+            Visible = true;
+        }
+
+        public void Explode()
+        {
+            if (rnd.NextDouble() > 0.95)
+            {
+                PowerUp pup = new PowerUp();
+                pup.Center = Center;
+                Root.AddChild(pup);
+            }
+            Explosion.Burst(Parent, Center);
+            Delete();
+        }
+
+        public override void DrawOn(Graphics graphics)
+        {
+            graphics.DrawImage(MiImagen, Bounds);
+        }
+        Image MiImagen;
+
+        private Image LoadImage()
+        {
+            Image ThisImage = Spritesheet.Ships[shipIndex];
+            Extent = new SizeF(ThisImage.Size.Width / 2, ThisImage.Size.Height / 2);
+            return ThisImage;
+        }
+    }
+}
---
+++ TP/Game/Player/PlayerShip.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using Engine;
+using Engine.Events;
+using System.Windows.Forms;
+using Engine.Extensions;
+using Engine.Utils;
+
+namespace Game
+{
+    public class PlayerShip : GameObject
+    {
+        public static readonly float MIN_SPEED = 200;
+        public static readonly float MAX_SPEED = 500;
+
+        private int shipIndex;
+        private PointF direction = new PointF(0, 0);
+        private HashSet<Keys> pressedKeys = new HashSet<Keys>();
+        private Cannon cannon;
+        private bool shieldActivated = false;
+        private float speed = MIN_SPEED;
+
+        
+
+
+        public PlayerShip(int shipIndex)
+        {
+            this.shipIndex = shipIndex;
+            LoadImage();
+
+            EventHandler.KeyDown += OnKeyDown;
+            EventHandler.KeyUp += OnKeyUp;
+
+            MiImagen = LoadImage();
+
+            cannon = new Cannon();
+            cannon.Center = Center;
+            cannon.Right = Right;
+            cannon.Visible = false;
+            AddChild(cannon);
+        }
+
+        public bool ShieldActivated
+        {
+            get { return shieldActivated; }
+            set { shieldActivated = value; }
+        }
+
+        public float Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public Cannon Cannon
+        {
+            get { return cannon; }
+        }
+
+        private void OnKeyUp(KeyUpEvent evt)
+        {
+            evt.Handled = false;
+            pressedKeys.Remove(evt.KeyData);
+        }
+
+        private void OnKeyDown(KeyDownEvent evt)
+        {
+            evt.Handled = false;
+            pressedKeys.Add(evt.KeyData);
+        }
+
+        public override void Update(float deltaTime)
+        {
+            float s = speed.MinMax(MAX_SPEED, MIN_SPEED);
+            if (pressedKeys.Contains(Keys.Up)
+                || pressedKeys.Contains(Keys.W))
+            {
+                direction.Y = -1 * s;
+            }
+            else if (pressedKeys.Contains(Keys.Down)
+                || pressedKeys.Contains(Keys.S))
+            {
+                direction.Y = 1 * s;
+            }
+            else
+            {
+                direction.Y = 0;
+            }
+
+            if (pressedKeys.Contains(Keys.Left)
+                || pressedKeys.Contains(Keys.A))
+            {
+                direction.X = -1 * s;
+            }
+            else if (pressedKeys.Contains(Keys.Right)
+                || pressedKeys.Contains(Keys.D))
+            {
+                direction.X = 1 * s;
+            }
+            else
+            {
+                direction.X = 0;
+            }
+
+
+            X += direction.X * deltaTime;
+            Y += direction.Y * deltaTime;
+            KeepInsideOwner();
+
+            
+            CheckForPowerUps();
+            if (CheckForCollision())
+            {
+                if (!shieldActivated)
+                {
+                    Explosion.Burst(Parent, Center, 1000, 30, 450, 2, 2, -1);
+                    Explosion.Burst(Parent, TopLeft, 500, 30, 400, 1, 3, -1);
+                    Explosion.Burst(Parent, TopRight, 500, 30, 400, 1, 3, -1);
+                    Explosion.Burst(Parent, BottomLeft, 500, 30, 400, 1, 3, -1);
+                    Explosion.Burst(Parent, BottomRight, 500, 30, 400, 1, 3, -1);
+                    Delete();
+                }
+            }
+            else if (pressedKeys.Contains(Keys.Space)
+                || pressedKeys.Contains(Keys.Enter))
+            {
+                Shoot();
+            }
+        }
+
+        private bool CheckForCollision()
+        {
+
+            EnemyShip[] EnemyCollisions = AllObjects.Select(m => m as EnemyShip).Where(m => m != null).ToArray();
+
+            if (EnemyCollisions.Count() == 0) return false;
+
+            foreach (var enemy in EnemyCollisions)
+            {
+                if (CollidesWith(enemy))
+                {
+                    enemy.Explode();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void CheckForPowerUps()
+        {
+
+            PowerUp[] pups = AllObjects.Select(m => m as PowerUp).ToArray();
+
+            foreach (PowerUp pup in pups)
+            {
+                if (pup != null)
+                {
+                    if (CollidesWith(pup))
+                    {
+                        pup.ApplyOn(this);
+                    }
+                }
+            }
+
+        }
+
+        private void KeepInsideOwner()
+        {
+            if (Left < Parent.Left) { Left = Parent.Left; }
+            else if (Right > Parent.Right) { Right = Parent.Right; }
+
+            if (Top < Parent.Top) { Top = Parent.Top; }
+            else if (Bottom > Parent.Bottom) { Bottom = Parent.Bottom; }
+        }
+
+        private void Shoot()
+        {
+            IEnumerable<Cannon> cannons = AllObjects
+                .Select((m) => m as Cannon)
+                .Where((m) => m != null);
+            foreach (Cannon cannon in cannons)
+            {
+                cannon.Shoot();
+            }
+        }
+
+        Image MiImagen;
+        public override void DrawOn(Graphics graphics)//Manda a dibujarme
+        {
+            graphics.DrawImage(MiImagen, Bounds);
+        }
+
+        private Image LoadImage()//Carga las imagenes y me devuelve la mia
+        {
+            Image[] ships = Spritesheet.Load(@"Resources\shipsheetparts.png", new Size(200, 200));
+            ships[shipIndex].RotateFlip(RotateFlipType.Rotate270FlipNone);
+            ships[shipIndex].RotateFlip(RotateFlipType.Rotate270FlipNone);
+            ships[shipIndex].RotateFlip(RotateFlipType.Rotate270FlipNone);
+            ships[shipIndex].RotateFlip(RotateFlipType.Rotate270FlipNone);
+            Image result = ships[shipIndex];
+            result.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            Extent = new SizeF(result.Size.Width / 2, result.Size.Height / 2);
+            return result;
+        }
+    }
+}
